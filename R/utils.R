dffd87895e53a9e9824a03852f995adbf205cb24
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-14 11:23:18 -0400

    Small refactoring of assert_internally()

--- R/utils.R
+++ R/utils.R
@@ -20,19 +20,20 @@ check_table <- function(x) {
   sprintf("%s is not a data frame", name)
 }
 
-assert_internally <- function(expr, ..., error = NULL) {
-  error <- error %||% function(err) {
-    message("An error occurred in the grading code: ", err$message)
-    gradethis::graded(
-      message = paste(
-        "Uh-oh! We can't provide feedback at this time. Don't worry, it's not", 
-        "your fault! There's an issue behind-the-scenes with this exercise."
-      ),
-      correct = logical(0),
-      type = "warning",
-      location = "replace",
-      problem = problem("internal_feedback_error", error = err$message)
-    )
-  }
+assert_internally <- function(expr, ..., error = internal_error) {
   tryCatch(expr, error = error, ...)
+}
+
+internal_error <- function(err) {
+  message("An error occurred in the grading code: ", err$message)
+  gradethis::graded(
+    message = paste(
+      "Uh-oh! We can't provide feedback at this time. Don't worry, it's not", 
+      "your fault! There's an issue behind-the-scenes with this exercise."
+    ),
+    correct = logical(0),
+    type = "warning",
+    location = "replace",
+    problem = problem("internal_feedback_error", error = err$message)
+  )
 }
