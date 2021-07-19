bd747c7e96ee2d875a68ddedda307383e26eb78a
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-07-19 15:44:15 -0700

    Pass dots in `expect_result_message()`

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,3 +1,3 @@
-expect_result_message <- function(result, expected) {
-  expect_match(as.character(result$feedback$message), expected)
+expect_result_message <- function(result, expected, ...) {
+  expect_match(as.character(result$feedback$message), expected, ...)
 }
