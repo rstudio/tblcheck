a635ef8b077b890ae996d04fbea18eae3e741302
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-23 15:39:26 -0400

    Prefix testthat functions in tests-helpers.R

--- R/tests-helpers.R
+++ R/tests-helpers.R
@@ -1,20 +1,20 @@
 expect_internal_problem <- function(grade, message) {
-  expect_message(grade)
-  expect_equal(grade$correct, logical())
-  expect_match(grade$message, "can't provide feedback")
-  expect_equal(grade$problem$type, "internal_feedback_error")
-  expect_match(as.character(grade$problem$error), message)
+  testthat::expect_message(grade)
+  testthat::expect_equal(grade$correct, logical())
+  testthat::expect_match(grade$message, "can't provide feedback")
+  testthat::expect_equal(grade$problem$type, "internal_feedback_error")
+  testthat::expect_match(as.character(grade$problem$error), message)
 }
 
 expect_result_message <- function(result, expected, ...) {
-  expect_match(as.character(result$feedback$message), expected, ...)
+  testthat::expect_match(as.character(result$feedback$message), expected, ...)
 }
 
 expect_grade <- function(grade, message, correct = FALSE, problem = NULL, ...) {
-  expect_s3_class(grade, "gradethis_graded")
-  expect_equal(grade$correct, correct)
-  expect_match(grade$message, message, ...)
+  testthat::expect_s3_class(grade, "gradethis_graded")
+  testthat::expect_equal(grade$correct, correct)
+  testthat::expect_match(grade$message, message, ...)
   if (!is.null(problem)) {
-    expect_equal(grade$problem, problem)
+    testthat::expect_equal(grade$problem, problem)
   }
 }
