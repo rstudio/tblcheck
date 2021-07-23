189d9942fa7234a536e0d2e90771040a4d5d37f3
Garrick Aden-Buie <garrick@adenbuie.com>
2021-07-23 14:57:39 -0400

    Move expect_result_message() into helper-expectations.R

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -5,3 +5,7 @@ expect_internal_problem <- function(grade, message) {
   expect_equal(grade$problem$type, "internal_feedback_error")
   expect_match(as.character(grade$problem$error), message)
 }
+
+expect_result_message <- function(result, expected, ...) {
+  expect_match(as.character(result$feedback$message), expected, ...)
+}
