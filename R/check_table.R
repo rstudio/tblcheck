f72a6b746e857994adc35835fd24adf7354fea06
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-08-10 14:40:04 -0700

    Update documentation linking

--- R/check_table.R
+++ R/check_table.R
@@ -15,14 +15,14 @@
 #' 4. `table_names`: The table has names that are not expected,
 #'   or is missing names that are expected.
 #' 
-#' Additional problems may be produced by [check_column()]
+#' Additional problems may be produced by [tbl_check_column()]
 #'
 #' @param object A data frame to be compared to `expected`.
 #' @param expected A data frame containing the expected result.
 #' @param max_diffs `[numeric(1)]`\cr The maximum number of mismatched values to
 #'   display in an informative failure message.
-#'   Passed to [check_names()] to determine the number of mismatched column
-#'   names to display and the `n_values` argument of [check_column()] to
+#'   Passed to [tbl_check_names()] to determine the number of mismatched column
+#'   names to display and the `n_values` argument of [tbl_check_column()] to
 #'   determine the number of mismatched column values to display.
 #'   Defaults to 3.
 #' @param check_class `[logical(1)]`\cr Whether to check that `object` and 
@@ -30,13 +30,13 @@
 #' @param check_nrow `[logical(1)]`\cr Whether to check that `object` and 
 #'   `expected` have the same number of rows with [nrow()].
 #' @param check_names `[logical(1)]`\cr Whether to check that `object` and
-#'   `expected` have the same column names with [check_names()].
+#'   `expected` have the same column names with [tbl_check_names()].
 #' @param check_ncol `[logical(1)]`\cr Whether to check that `object` and
 #'   `expected` have the same number of columns with [ncol()].
 #'   By default, `check_ncol` is [`FALSE`] if `check_names` is [`TRUE`], as
 #'   running both is redundant.
 #' @param check_columns `[logical(1)]`\cr Whether to check that all columns
-#'   have the same contents with [check_column()].
+#'   have the same contents with [tbl_check_column()].
 #' @param check_column_class `[logical(1)]`\cr Whether to check that each
 #'   columns has the same class in `object` and `expected`.
 #' @param check_column_values `[logical(1)]`\cr Whether to check that each
