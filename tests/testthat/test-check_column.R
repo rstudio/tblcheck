04ba563d678dc50a46334c3be823bafcdaf9a51e
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-08-25 15:29:26 -0700

    Revert "Move to snapshot testing"
    This reverts commit 797eb9e2d9a92d85dfd79156bdf338d96c307f5a.

--- tests/testthat/test-check_column.R
+++ tests/testthat/test-check_column.R
@@ -2,88 +2,189 @@ test_that("tbl_grade_column() checks classes", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters)
     .solution <- tibble::tibble(a = 1:3)
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a", .result, .solution)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "column_class",
+      "integer", 
+      "character",
+      expected_length = 3,
+      actual_length = 26,
+      column = "a"
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    "Your `a` column should be a vector of integers (class `integer`), but it is a vector of text (class `character`).",
+    problem = problem,
+    fixed = TRUE
+  )
 })
 
 test_that("tbl_grade_column() checks the first three values", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = rev(letters))
     .solution <- tibble::tibble(a = letters)
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a")
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "column_value_diffs",
+      letters[1:3],
+      column = "a"
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    "The first 3 values of your `a` column should be `a`, `b`, and `c",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_column() checks multiple classes", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = data.frame(x = 1))
     .solution <- tibble::tibble(a = tibble::tibble(x = 1))
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a")
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      type = "column_class", 
+      expected = c("tbl_df", "tbl", "data.frame"), 
+      actual = "data.frame",
+      expected_length = 1,
+      actual_length = 1,
+      column = "a"
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    "Your `a` column should be a tibble (class `tbl_df`), but it is a data frame (class `data.frame`).",
+    problem = problem,
+    fixed = TRUE
+  )
 })
 
 test_that("tbl_grade_column() checks for value differences beyond the first 3", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = c(rep(1, 3), 5:10))
     .solution <- tibble::tibble(a = c(rep(1, 3), 10:15))
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a")
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem, 
+    problem("column_values", column = "a"),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    "Your `a` column contains unexpected values.",
+    problem = problem
+  )
 })
 
 test_that("max_diffs modifies the number of values to print", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters)
     .solution <- tibble::tibble(a = rev(letters))
+    problem   <- tbl_check_column("a", max_diffs = 5)
     tbl_grade_column("a", max_diffs = 5)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("column_value_diffs", letters[26:22], column = "a"),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    "The first 5 values of your `a` column should be `z`, `y`, `x`, `w`, and `v",
+    problem = problem
+  )
 })
 
 test_that("max_diffs doesn't overflow", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters[1:2])
     .solution <- tibble::tibble(a = letters[2:1])
+    problem   <- tbl_check_column("a", max_diffs = 3)
     tbl_grade_column("a", max_diffs = 3)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("column_value_diffs", letters[2:1], column = "a"),
+    ignore_attr = "class"
+  )
+
+  expect_grade(
+    grade,
+    message = "`b` and `a`.",
+    problem = problem
+  )
+  expect_no_match(grade$message, "`NA`")
 })
 
 test_that("checks that columns have the same length", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters[1:3])
     .solution <- tibble::tibble(a = letters[1:4])
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a")
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("column_length", 4, 3, column = "a"),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should contain 4 values",
+    problem = problem
+  )
 })
 
 test_that("checks that the column is present in object", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(b = letters[1:3])
     .solution <- tibble::tibble(a = letters[1:3])
+    problem   <- tbl_check_column("a")
     tbl_grade_column("a")
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("column_missing", "a", column = "a"),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should have a column named `a`",
+    problem = problem
+  )
 })
 
 test_that("checks that the column is present in expected", {
@@ -92,12 +193,19 @@ test_that("checks that the column is present in expected", {
       grade <- tblcheck_test_grade({
         .result   <- tibble::tibble(b = letters[1:3])
         .solution <- tibble::tibble(a = letters[1:3])
+        expect_warning(
+          {
+            problem <- tbl_check_column("b")
+          },
+          "`b` is not a column in `expected`"
+        )
         tbl_grade_column("b")
       })
     },
     "`b` is not a column in `expected`"
   )
   
+  expect_null(problem)
   expect_null(grade)
 })
 
@@ -105,9 +213,11 @@ test_that("tbl_grade_column() with no problems returns invisible()", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters[1:3])
     .solution <- tibble::tibble(a = letters[1:3])
+    problem   <- expect_invisible(tbl_check_column("a"))
     tbl_grade_column("a")
   })
   
+  expect_null(problem)
   expect_null(grade)
   
   expect_invisible(
--- tests/testthat/test-check_table.R
+++ tests/testthat/test-check_table.R
@@ -2,100 +2,233 @@ test_that("tbl_grade_table() class", {
   grade <- tblcheck_test_grade({
     .result   <- data.frame(a = 1:10, b = 1:10)
     .solution <- tibble::tibble(a = 1:10, b = 1:10)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_class",
+      c("tbl_df", "tbl", "data.frame"),
+      "data.frame",
+      expected_length = 2,
+      actual_length = 2
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "Your table should be a tibble (class `tbl_df`), but it is a data frame (class `data.frame`)",
+    problem = problem,
+    fixed = TRUE
+  )
   
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = 1:10, b = a)
     .solution <- dplyr::group_by(tibble::tibble(a = 1:10, b = a), a)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_class",
+      c("grouped_df", "tbl_df", "tbl", "data.frame"),
+      c("tbl_df", "tbl", "data.frame"),
+      expected_length = 2,
+      actual_length = 2
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "Your table isn't a grouped data frame, but I was expecting it to be grouped. Maybe you need to use `group_by()`?",
+    problem = problem,
+    fixed = TRUE
+  )
   
   grade <- tblcheck_test_grade({
     .result   <- dplyr::rowwise(tibble::tibble(a = 1:10, b = a))
     .solution <- tibble::tibble(a = 1:10, b = a)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_class",
+      c("tbl_df", "tbl", "data.frame"),
+      c("rowwise_df", "tbl_df", "tbl", "data.frame"),
+      expected_length = 2,
+      actual_length = 2
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "Your table is a rowwise data frame, but I wasn't expecting it to be rowwise. Maybe you need to use `ungroup()`?",
+    problem = problem,
+    fixed = TRUE
+  )
   
   grade <- tblcheck_test_grade({
     .result   <- dplyr::rowwise(tibble::tibble(a = 1:10, b = a))
     .solution <- dplyr::group_by(tibble::tibble(a = 1:10, b = a), a)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_class",
+      c("grouped_df", "tbl_df", "tbl", "data.frame"),
+      c("rowwise_df", "tbl_df", "tbl", "data.frame"),
+      expected_length = 2,
+      actual_length = 2
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "Your table is a rowwise data frame, but I was expecting it to be grouped. Maybe you need to use `group_by()`?",
+    problem = problem,
+    fixed = TRUE
+  )
 })
 
 test_that("tbl_grade_table() rows", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a)
     .solution <- tibble::tibble(a = letters[-1], b = a)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_nrow", 25, 26),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should have 25 rows",
+    problem = problem
+  )
   
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a)
     .solution <- tibble::tibble(a = letters[1], b = a)
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_nrow", 1, 26),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should have 1 row",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_table() ncol", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a, c = a)
     .solution <- tibble::tibble(a = letters, b = a)
+    problem   <- tbl_check_table(check_names = FALSE)
     tbl_grade_table(check_names = FALSE)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_ncol", 2, 3),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should have 2 columns",
+    problem = problem
+  )
   
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a, c = a)
     .solution <- tibble::tibble(a = letters)
+    problem   <- tbl_check_table(check_names = FALSE)
     tbl_grade_table(check_names = FALSE)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_ncol", 1, 3),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "should have 1 column",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_table() names", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters[1:3], b = a)
     .solution <- tibble::tibble(x = letters[1:3], y = x)
+    problem   <- tbl_check_table(object = .result, expected = .solution)
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_names",
+      missing = c("x", "y"),
+      unexpected = c("a", "b")
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "Your table should have columns named `x` and `y`. Your table should not have columns named `a` or `b`.",
+    problem = problem,
+    fixed = TRUE
+  )
 })
 
 test_that("tbl_grade_table() columns", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters[1:3])
     .solution <- tibble::tibble(a = letters[24:26])
+    problem   <- tbl_check_table(object = .result, expected = .solution)
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("column_value_diffs", letters[24:26], column = "a"),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade,
+    message = "first 3 values of your `a` column should be `x`, `y`, and `z`",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_table() with no problems returns invisible()", {
@@ -117,40 +250,80 @@ test_that("tbl_grade_table() returns grades with row problems", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters)
     .solution <- tibble::tibble(a = letters[1:25])
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_nrow", 25, 26),
+    ignore_attr = "class"
+  )
   
-  grade <- tblcheck_test_grade({
+  expect_grade(
+    grade,
+    "should have 25 rows",
+    problem = problem
+  )
+  
+  grade_single <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters)
     .solution <- tibble::tibble(a = letters[1])
+    problem   <- tbl_check_table()
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_nrow", 1, 26),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade_single,
+    "should have 1 row",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_table() returns ncol feedback to learnr", {
   grade <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = letters, c = letters)
     .solution <- tibble::tibble(a = letters, b = letters)
+    problem   <- tbl_check_table(check_names = FALSE)
     tbl_grade_table(check_names = FALSE)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_ncol", 2, 3),
+    ignore_attr = "class"
+  )
   
-  grade <- tblcheck_test_grade({
+  expect_grade(
+    grade,
+    "should have 2 columns",
+    problem = problem
+  )
+  
+  grade_one <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = letters, c = letters)
     .solution <- tibble::tibble(a = letters)
+    problem   <- tbl_check_table(check_names = FALSE)
     tbl_grade_table(check_names = FALSE)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem("table_ncol", 1, 3),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade_one,
+    "should have 1 column",
+    problem = problem
+  )
 })
 
 test_that("tbl_grade_table() returns names feedback to learnr", {
@@ -161,28 +334,84 @@ test_that("tbl_grade_table() returns names feedback to learnr", {
     tbl_grade_table()
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem, 
+    problem(
+      "table_names",
+      missing = c("x", "y", "z", "w"),
+      unexpected = c("a", "b", "c", "d")
+    ),
+    ignore_attr = "class"
+  )
+
+  expect_grade(
+    grade, 
+    "Your table should have columns named .*x.*, .*y.*, .*z.*, and 1 more",
+    problem = problem
+  )
+  
+  expect_grade(
+    grade,
+    "Your table should not have columns named .*a.*, .*b.*, .*c.*, or 1 more"
+  )
   
   # ---- with all diffs ---
-  grade <- tblcheck_test_grade({
+  grade_inf <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a, c = a, d = a)
     .solution <- tibble::tibble(x = letters, y = x, z = x, w = x)
+    problem   <- tbl_check_table(max_diffs = Inf)
     tbl_grade_table(max_diffs = Inf)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_names",
+      missing = c("x", "y", "z", "w"),
+      unexpected = c("a", "b", "c", "d")
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade_inf,
+    "Your table should have columns named .*x.*, .*y.*, .*z.*, and .*w",
+    problem = problem
+  )
+  
+  expect_grade(
+    grade_inf,
+    "Your table should not have columns named .*a.*, .*b.*, .*c.*, or .*d"
+  )
   
   # ---- with one diff ---
-  grade <- tblcheck_test_grade({
+  grade_one <- tblcheck_test_grade({
     .result   <- tibble::tibble(a = letters, b = a, c = a, d = a)
     .solution <- tibble::tibble(x = letters, y = x, z = x, w = x)
+    problem   <- tbl_check_table(max_diffs = 1)
     tbl_grade_table(max_diffs = 1)
   })
   
-  expect_snapshot(grade)
-  expect_snapshot_value(grade$problem, style = "deparse")
+  expect_equal(
+    problem,
+    problem(
+      "table_names",
+      missing = c("x", "y", "z", "w"),
+      unexpected = c("a", "b", "c", "d")
+    ),
+    ignore_attr = "class"
+  )
+  
+  expect_grade(
+    grade_one,
+    "Your table should have columns named .*x.* and 3 more",
+    problem = problem
+  )
+  
+  expect_grade(
+    grade_one,
+    "Your table should not have columns named .*a.* or 3 more"
+  )
 })
 
 test_that("tbl_grade_table() handles bad user input", {
