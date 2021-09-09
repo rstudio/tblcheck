25a0e0697f5861c9ddbd154de45db3c397422dd3
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-09-09 11:59:55 -0700

    Add `vec_check_values()` so `vec_check_vector()` no longer generate its own problems

--- R/check_values.R
+++ R/check_values.R
@@ -1,21 +1,16 @@
-#' Checks that two vectors are the same
+#' Checks that two vectors are contain the same values
 #'
-#' Check if two vectors have the same class, length, and values.
-#' If the vectors differ
-#' - `vec_check_vector()` returns a list describing the problem
-#' - `vec_grade_vector()` returns a failing grade and informative message
+#' Check if two vectors contain the same values.
+#' If the values differ
+#' - `vec_check_values()` returns a list describing the problem
+#' - `vec_grade_values()` returns a failing grade and informative message
 #' with [gradethis::fail()]
 #' 
 #' @section Problems:
 #' 
-#' 1. `vector_class`: `object` doesn't have the same classes as `expected`
-#' 1. `vector_length`: `object` doesn't have the same length as `expected`
-#' 1. `vector_n_levels`, `vector_levels`, `vector_level_order_diffs`,
-#'   `vector_level_order`: See [vec_check_levels()]
-#' 1. `column_value_diffs`: The first `max_diffs` elements of `object` don't
+#' 1. `value_diffs`: The first `max_diffs` elements of `object` don't
 #'   contain the same values as `expected`
-#' 1. `vector_values`: `object` doesn't contain the same values as `expected`
-#' 1. `vector_names`: `object` has different [names][names()] than `expected`
+#' 1. `values`: `object` doesn't contain the same values as `expected`
 #'
 #' @param object A vector to be compared to `expected`.
 #' @param expected A vector containing the expected result.
@@ -62,15 +57,10 @@
 #' vec_grade_vector()
 #' vec_grade_vector(max_diffs = 5)
 #' vec_grade_vector(max_diffs = Inf)
-vec_check_vector <- function(
+vec_check_values <- function(
   object = .result,
   expected = .solution,
   max_diffs = 3,
-  check_class = TRUE,
-  check_length = TRUE,
-  check_levels = TRUE,
-  check_values = TRUE,
-  check_names = TRUE,
   env = parent.frame()
 ) {
   if (inherits(object, ".result")) {
@@ -84,57 +74,29 @@ vec_check_vector <- function(
     checkmate::assert_vector(object)
     checkmate::assert_vector(expected)
     checkmate::assert_number(max_diffs, lower = 1)
-    checkmate::assert_logical(check_class,  any.missing = FALSE, len = 1)
-    checkmate::assert_logical(check_values, any.missing = FALSE, len = 1)
-    checkmate::assert_logical(check_length, any.missing = FALSE, len = 1)
   })
   
-  if (check_class) {
-    return_if_problem(
-      vec_check_class(object, expected),
-      prefix = "vector"
-    )
+  exp_values <- unname(expected)
+  obj_values <- unname(object)
+  
+  n_values <- min(length(expected), max_diffs)
+  first_n_values <- exp_values[seq_len(n_values)]
+  
+  if (!identical(obj_values[seq_len(n_values)], first_n_values)) {
+    return(problem("value_diffs", first_n_values))
   }
   
-  if (check_length) {
-    return_if_problem(
-      vec_check_length(object, expected),
-      prefix = "vector"
-    )
-  }
-  
-  if (check_levels) {
-    return_if_problem(
-      vec_check_levels(object, expected),
-      prefix = "vector"
-    )
-  }
-  
-  if (check_values) {
-    return_if_problem(
-      vec_check_values(object, expected, max_diffs),
-      prefix = "vector"
-    )
-  }
-  
-  if (check_names) {
-    return_if_problem(
-      vec_check_names(object, expected),
-      prefix = "vector"
-    )
+  if (!identical(obj_values, exp_values)) {
+    return(problem("values"))
   }
 }
 
-#' @rdname vec_check_vector
+#' @rdname vec_check_values
 #' @export
-vec_grade_vector <- function(
+vec_grade_values <- function(
   object = .result,
   expected = .solution,
   max_diffs = 3,
-  check_class = TRUE,
-  check_length = TRUE,
-  check_values = TRUE,
-  check_names = TRUE,
   env = parent.frame()
 ) {
   return_if_graded(
@@ -143,14 +105,43 @@ vec_grade_vector <- function(
         object = object,
         expected = expected,
         max_diffs = max_diffs,
-        check_class = check_class,
-        check_length = check_length,
-        check_values = check_values,
-        check_names = check_names,
         env = env
       ),
       max_diffs = max_diffs,
       env = env
     )
   )
+}
+
+tbl_message.value_diffs_problem <- function(problem, ...) {
+  problem$n_values <- length(problem$expected)
+  problem$expected <- knitr::combine_words(md_code(problem$expected))
+  
+  problem$msg <- problem$msg %||%
+    ngettext(
+      problem$n_values,
+      "The first value of your result should be {expected}.",
+      "The first {n_values} values of your result should be {expected}."
+    )
+  
+  glue::glue_data(problem, problem$msg)
+}
+
+tbl_message.column_value_diffs_problem <- function(problem, ...) {
+  problem$msg <- problem$msg %||%
+    ngettext(
+      length(problem$expected),
+      "The first value of your `{column}` column should be {expected}.",
+      "The first {n_values} values of your `{column}` column should be {expected}."
+    )
+  
+  NextMethod()
+}
+
+tbl_message.values_problem <- function(problem, ...) {
+  "Your result contains unexpected values."
+}
+
+tbl_message.column_values_problem <- function(problem, ...) {
+  glue::glue_data(problem, "Your `{column}` column contains unexpected values.")
 }
