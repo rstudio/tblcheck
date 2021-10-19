4cf9bd24bb32abf139a6130f41bb363b5f29789c
Garrick Aden-Buie <garrick@adenbuie.com>
2021-10-19 11:26:26 -0400

    Add `grade_this_*()` tblcheck grading functions

---
+++ R/grade_this.R
@@ -0,0 +1,269 @@
+#' Grade This Table or Vector
+#' 
+#' Automatically grade a table or vector resulting from student code, using the
+#' author's solution as a reference.
+#'   
+#' @examples
+#  <!-- TODO: improve these examples --> 
+#' ex <- mock_this_exercise(
+#'   .solution_code = tibble(x = 1:3, y = letters[x]),
+#'   .user_code = tibble(x = 1:3, y = c("A", "b", "c"))
+#' )
+#' 
+#' ## Grading Tables ----
+#' grade_this_table()(ex)
+#' 
+#' # Roughly equivalent to...
+#' gradethis::grade_this({
+#'   gradethis::pass_if_equal()
+#'   tbl_grade()
+#'   gradethis::fail()
+#' })(ex)
+#' 
+#' ## Grading Columns in Tables ----
+#' grade_this_column("y")(ex)
+#' 
+#' ## Grading Vectors ----
+#' # Here we use `pre_check` to modify `.result` and
+#' grade_this_vector(
+#'   pre_check = {
+#'     .result <- .result$y
+#'     .solution <- .solution$y
+#'   }
+#' )(ex)
+#' 
+#' @param correct `[character(1)]`\cr The message shown to the student when
+#'   their `.result` matches the exercise `.solution`, if `pass_if_equal` is
+#'   `TRUE`.
+#' @param pre_check,post_check `[expression]`\cr Code to run before or after the
+#'   table or vector grading is performed. The pre check runs before calling
+#'   [gradethis::pass_if_equal()] so that you can modify or adjust the student's
+#'   `.result` or the `.solution` if there are parts of either that need to be
+#'   ignored. These arguments can also be used in conjunction with the
+#'   `pass_if_equal` option when the grading requirements are more involved.
+#' @param pass_if_equal `[logical(1)]`\cr When `TRUE` (default), the `.result`
+#'   is compared to the `.solution` with [gradethis::pass_if_equal()] after the
+#'   _pre check_ and before calling the \pkg{tblcheck} grading function. When
+#'   `FALSE`, you will need to include your own call to [gradethis::pass()] in
+#'   either `pre_check` or `post_check` for the student to be able to receive a
+#'   passing grade.
+#' @inheritParams tbl_grade
+#' @inheritParams vec_grade
+#' @inheritParams gradethis::fail
+#' @inheritParams gradethis::gradethis_setup
+#' 
+#' @return The returned feedback is equivalent to \pkg{gradethis} grading code
+#'   using [`grade_this()`][gradethis::grade_this] with the following 
+#'   components:
+#'   
+#'   1. First the `pre_check` code, if any, is evaluated. If this code calls
+#'      [`pass()`][gradethis::graded], [`fail()`][gradethis::graded], or their
+#'      equivalents, that feedback is provided immediately.
+#'   2. If `pass_if_equal` is `TRUE`, then 
+#'      [`pass_if_equal()`][gradethis::pass_if_equal] is called to compare the
+#'      [`.result`][gradethis::grade_this-objects] to the 
+#'      [`.solution`][gradethis::grade_this-objects]. The message in `correct`
+#'      is used for the feedback.
+#'   3. The appropriate \pkg{tblcheck} grading function is called, returning
+#'      any feedback:
+#'       1. `grade_this_table()` returns the results from [tbl_grade()]
+#'       2. `grade_this_column()` returns the results from [tbl_grade_column()]
+#'       3. `grade_this_vector()` returns the results from [vec_grade()]
+#'   4. The `post_check` code, if any, is evaluated and any feedback from a call
+#'      to [`pass()`][gradethis::graded], [`fail()`][gradethis::graded], or 
+#'      their equivalents is returned.
+#'   5. Finally, if no other feedback is returned, the feedback from 
+#'      [gradethis::fail()] is provided to the student, using the options
+#'      `fail.message`, `fail.hint` and `fail.encourage`.
+#' 
+#' @name tblcheck_grade_this
+#' @export
+NULL
+
+#' @describeIn tblcheck_grade_this Grade a table using [tbl_grade()]
+#' @export
+grade_this_table <- function(
+  correct = NULL,
+  pre_check = NULL,
+  post_check = NULL,
+  pass_if_equal = TRUE,
+  ...,
+  # all the arguments from tbl_grade_table() except object/expected
+  max_diffs = 3,
+  check_class = TRUE,
+  check_names = TRUE,
+  check_dimensions = TRUE,
+  check_groups = TRUE,
+  check_columns = TRUE,
+  check_column_class = check_columns,
+  check_column_values = check_columns,
+  hint = getOption("gradethis.fail.hint", FALSE),
+  encourage = getOption("gradethis.fail.encourage", FALSE),
+  # gradethis pass/fail options
+  pass.praise = NULL,
+  fail.message = NULL,
+  fail.hint = hint, 
+  fail.encourage = encourage
+) {
+  ellipsis::check_dots_empty()
+  grader <- call2_tblcheck_grade_this(tbl_grade)
+  rlang::eval_bare(grader)
+}
+
+#' @describeIn tblcheck_grade_this Grade a column in a table using
+#'   [tbl_grade_column()]
+#' @export
+grade_this_column <- function(
+  column,
+  correct = NULL,
+  pre_check = NULL,
+  post_check = NULL,
+  pass_if_equal = TRUE,
+  ...,
+  # all the arguments from tbl_grade_column() except object/expected
+  max_diffs = 3,
+  check_class = TRUE,
+  check_length = TRUE,
+  check_values = TRUE,
+  check_names = FALSE,
+  hint = getOption("gradethis.fail.hint", FALSE),
+  encourage = getOption("gradethis.fail.encourage", FALSE),
+  # gradethis pass/fail options
+  pass.praise = NULL,
+  fail.message = NULL,
+  fail.hint = hint, 
+  fail.encourage = encourage
+)  {
+  ellipsis::check_dots_empty()
+  grader <- call2_tblcheck_grade_this(tbl_grade_column)
+  rlang::eval_bare(grader)
+}
+
+#' @describeIn tblcheck_grade_this Grade a vector using [vec_grade()]
+#' @export
+grade_this_vector <- function(
+  correct = NULL,
+  pre_check = NULL,
+  post_check = NULL,
+  pass_if_equal = TRUE,
+  ...,
+  # all the arguments from tbl_grade_table() except object/expected
+  max_diffs = 3,
+  check_class = TRUE,
+  check_length = TRUE,
+  check_values = TRUE,
+  check_names = TRUE,
+  hint = getOption("gradethis.fail.hint", FALSE),
+  encourage = getOption("gradethis.fail.encourage", FALSE),
+  # gradethis pass/fail options
+  pass.praise = NULL,
+  fail.message = NULL,
+  fail.hint = hint, 
+  fail.encourage = encourage
+) {
+  ellipsis::check_dots_empty()
+  grader <- call2_tblcheck_grade_this(vec_grade)
+  rlang::eval_bare(grader)
+}
+
+call2_tblcheck_grade_this <- function(
+  tblcheck_grader = tbl_grade,
+  call_frame = NULL
+) {
+  call_frame <- call_frame %||% rlang::call_frame(n = 2)
+  
+  # take args of the function calling this one (or the one in the call frame)
+  call <- call_frame$expr
+  call <- rlang::call_match(call, fn = rlang::call_fn(call), defaults = TRUE)
+  args <- rlang::call_args(call_frame$expr)
+  
+  if ("..." %in% names(args)) {
+    # drop the `...` that shouldn't have been used anyway
+    args <- args[setdiff(names(args), "...")]
+  }
+  
+  # add the tblcheck grader to the arg list
+  args$tblcheck_grader <- rlang::enexpr(tblcheck_grader)
+  
+  # and construct the call to the general purpose `tblcheck_grade_this()`
+  rlang::call2("tblcheck_grade_this", !!!args)
+}
+
+tblcheck_grade_this <- function(
+  tblcheck_grader = tbl_grade,
+  ...,
+  correct = NULL,
+  pre_check = NULL,
+  post_check = NULL,
+  pass_if_equal = TRUE,
+  hint = getOption("gradethis.fail.hint", FALSE),
+  encourage = getOption("gradethis.fail.encourage", FALSE),
+  # gradethis pass/fail options
+  pass.praise = NULL,
+  fail.message = NULL,
+  fail.hint = hint, 
+  fail.encourage = encourage
+) {
+  pre_check  <- rlang::enexpr(pre_check)
+  post_check <- rlang::enexpr(post_check)
+  
+  tblcheck_grader_args <- list(
+    object = .result,
+    expected = .solution,
+    hint = hint,
+    encourage = encourage,
+    ...
+  )
+  
+  function(check_env) {
+    check_env_pre <- rlang::new_environment(
+      list(
+        ".__tblcheck_grader_args" = tblcheck_grader_args,
+        ".__tblcheck_grader" = tblcheck_grader,
+        ".__pre_check"       = pre_check,
+        ".__post_check"      = post_check,
+        ".__pass_if_equal"   = pass_if_equal,
+        ".__correct"         = correct        %||% getOption("gradethis.pass"),
+        ".__pass.praise"     = pass.praise    %||% getOption("gradethis.pass.praise", FALSE),
+        ".__fail.message"    = fail.message   %||% getOption("gradethis.fail.message"),
+        ".__fail.hint"       = fail.hint      %||% getOption("gradethis.fail.hint", FALSE),
+        ".__fail.encourage"  = fail.encourage %||% getOption("gradethis.fail.encourage", FALSE)
+      ),
+      parent = rlang::env_parent(check_env)
+    )
+    # insert the pre-check env one level above check_env
+    rlang::env_poke_parent(check_env, check_env_pre)
+    
+    grade <- gradethis::grade_this({
+      # Evaluate pre-check code
+      pre_check <- get0(".__pre_check")
+      if (!is.null(pre_check)) {
+        rlang::eval_bare(pre_check)
+      }
+      
+      if (isTRUE(get0(".__pass_if_equal", inherits = TRUE, ifnotfound = FALSE))) {
+        # pass immediately if they're _exactly_ the same
+        gradethis::pass_if_equal(message = get(".__correct"), praise = get(".__pass.praise"))
+      }
+      
+      # call the tblcheck grader
+      do.call(get(".__tblcheck_grader"), get(".__tblcheck_grader_args"))
+      
+      # evaluate post check or extra check code
+      post_check <- get0(".__post_check")
+      if (!is.null(post_check)) {
+        rlang::eval_bare(post_check)
+      }
+      
+      # finally, worst case scenario, fail()
+      fail(
+        message = get(".__fail.message"),
+        hint = get(".__fail.hint"),
+        encourage = get(".__fail.encourage")
+      )
+    })(check_env)
+    
+    class(grade) <- c("tblcheck_graded", class(grade))
+    grade
+  }
+}
