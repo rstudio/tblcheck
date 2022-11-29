525356508b8a72fd74462230e6028073b5de4bd9
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-11-29 12:21:44 -0800

    refactor(hinted_class_message): use `S4` instead of `R7`

--- R/check_class-hinted_class_message.R
+++ R/check_class-hinted_class_message.R
@@ -1,88 +1,99 @@
-hinted_class_message <- R7::new_generic(
-	"hinted_class_message",
-	c("object", "expected")
+#' Generate a hint for how to convert one object type to another
+#'
+#' @param object An object to be compared to `expected`
+#' @param expected An object of the expected class
+#'
+#' @return A [character] string of length 1
+#' @export
+setGeneric("hinted_class_message", function(object, expected) {
+	standardGeneric("hinted_class_message")
+})
+
+#' @export
+setMethod("hinted_class_message", signature("ANY", "ANY"),
+	function(object, expected) {
+		NULL
+	}
 )
 
-R7::method(
-	hinted_class_message,
-	list(R7::class_any, R7::class_any)
-) <- function(object, expected) {
-	NULL
-}
+setOldClass("rowwise_df")
+setOldClass("grouped_df")
+#' @export
+setMethod("hinted_class_message", signature("rowwise_df", "grouped_df"),
+	function(object, expected) {
+		paste(
+			"Your table is a rowwise data frame,",
+			"but I was expecting it to be grouped.",
+			"Maybe you need to use `group_by()`?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("rowwise_df"), R7::new_S3_class("grouped_df"))
-) <- function(object, expected) {
-	paste(
-		"Your table is a rowwise data frame,",
-		"but I was expecting it to be grouped.",
-		"Maybe you need to use `group_by()`?"
-	)
-}
+setOldClass("data.frame")
+#' @export
+setMethod("hinted_class_message", signature("data.frame", "grouped_df"),
+	function(object, expected) {
+		paste(
+			"Your table isn't a grouped data frame,",
+			"but I was expecting it to be grouped.",
+			"Maybe you need to use `group_by()`?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("data.frame"), R7::new_S3_class("grouped_df"))
-) <- function(object, expected) {
-	paste(
-		"Your table isn't a grouped data frame,",
-		"but I was expecting it to be grouped.",
-		"Maybe you need to use `group_by()`?"
-	)
-}
+#' @export
+setMethod("hinted_class_message", signature("grouped_df", "data.frame"),
+	function(object, expected) {
+		paste(
+			"Your table is a grouped data frame,",
+			"but I wasn't expecting it to be grouped.",
+			"Maybe you need to use `ungroup()`?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("grouped_df"), R7::new_S3_class("data.frame"))
-) <- function(object, expected) {
-	paste(
-		"Your table is a grouped data frame,",
-		"but I wasn't expecting it to be grouped.",
-		"Maybe you need to use `ungroup()`?"
-	)
-}
+#' @export
+setMethod("hinted_class_message", signature("data.frame", "rowwise_df"),
+	function(object, expected) {
+		paste(
+			"Your table isn't a rowwise data frame,",
+			"but I was expecting it to be rowwise.",
+			"Maybe you need to use `rowwise()`?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("data.frame"), R7::new_S3_class("rowwise_df"))
-) <- function(object, expected) {
-	paste(
-		"Your table isn't a rowwise data frame,",
-		"but I was expecting it to be rowwise.",
-		"Maybe you need to use `rowwise()`?"
-	)
-}
+#' @export
+setMethod("hinted_class_message", signature("rowwise_df", "data.frame"),
+	function(object, expected) {
+		paste(
+			"Your table is a rowwise data frame,",
+			"but I wasn't expecting it to be rowwise.",
+			"Maybe you need to use `ungroup()`?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("rowwise_df"), R7::new_S3_class("data.frame"))
-) <- function(object, expected) {
-	paste(
-		"Your table is a rowwise data frame,",
-		"but I wasn't expecting it to be rowwise.",
-		"Maybe you need to use `ungroup()`?"
-	)
-}
+setOldClass("py_tbl_df")
+setOldClass("py_grouped_df")
+#' @export
+setMethod("hinted_class_message", signature("py_tbl_df", "py_grouped_df"),
+	function(object, expected) {
+		paste(
+			"I was only expecting 1 value for each grouping in the table," ,
+			"but you have multiple values per grouping.",
+			"Maybe you are missing a `.groupby()` call?"
+		)
+	}
+)
 
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("py_tbl_df"), R7::new_S3_class("py_grouped_df"))
-) <- function(object, expected) {
-	paste(
-		"I was only expecting 1 value for each grouping in the table," ,
-		"but you have multiple values per grouping.",
-		"Maybe you are missing a `.groupby()` call?"
-	)
-}
-
-R7::method(
-	hinted_class_message,
-	list(R7::new_S3_class("py_grouped_df"), R7::new_S3_class("py_tbl_df"))
-) <- function(object, expected) {
-	paste(
-		"Your table row labels (i.e. index) are not a numbered sequence.",
-		"You can tell by the extra spacing around the column names.",
-		"You can fix this with `.reset_index()`"
-	)
-}
+#' @export
+setMethod("hinted_class_message", signature("py_grouped_df", "py_tbl_df"),
+	function(object, expected) {
+		paste(
+			"Your table row labels (i.e. index) are not a numbered sequence.",
+			"You can tell by the extra spacing around the column names.",
+			"You can fix this with `.reset_index()`"
+		)
+	}
+)
